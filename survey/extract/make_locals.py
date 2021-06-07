2843c2fcc68eeca977fd32100053a37f3883c83a
plan-do-break-fix <jeffdswan@outlook.com>
2021-06-07 18:19:30 -0500

    feature(extract): added filter method for removing false positives

--- survey/extract/make_locals.py
+++ survey/extract/make_locals.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 import mysql.connector
 from mysql.connector import errorcode
+import re
 import sqlite3
 from typing import Tuple
 
@@ -23,6 +24,18 @@ def extract(cursor, pk: int) -> Tuple:
                    (pk,))
     return cursor.fetchone()
 
+def good_text(text):
+    if (not bool(text) or
+          ("404 not found" in text.lower().split("\n")[0] 
+           or "forbidden" in text.lower().split("\n")[0]
+           or "does not exist" in text.lower().split("\n")[0])
+        or
+          (text.startswith("<") and not text.startswith("<<")) # << is never a tag
+        or
+          len(text) < 8):
+        return False
+    return True
+
 def make_locals(db_path, mysql_host, user=None, passwd=None, _i=1, _f=1000000):
     sqlite_conn = sqlite3.connect(db_path)
     sqlite_c = sqlite_conn.cursor()
@@ -38,9 +51,8 @@ def make_locals(db_path, mysql_host, user=None, passwd=None, _i=1, _f=1000000):
         data = extract(mysql_c, _i)
         sqlite_c.execute("INSERT INTO master_list (domain) VALUES (?)", (data[0],))
         status, content_type = data[1], data[2]
-        if status == 200 and content_type == "txt":
+        if status == 200 and content_type == "txt" and good_text(data[3]):
             sqlite_c.execute("INSERT INTO text (text, domain) VALUES (?,?)", (data[3], _i))
         sqlite_conn.commit()
         print(f"{_i} | {data[3]}/humans.txt")
         _i += 1
-
