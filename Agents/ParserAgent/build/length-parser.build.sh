79afa68ee52d63d5fbcde66a8ddc14c20f44d1fe
plan-do-break-fix <jeffdswan@outlook.com>
2022-02-08 21:31:50 -0600

    (agents) LengthParser build script - added argument validation & feedback to stdout

--- Agents/ParserAgent/build/length-parser.build.sh
+++ Agents/ParserAgent/build/length-parser.build.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 # Script for building LengthParser Agent and pushing to ECR repo
 
-# TODO - local vars needed:
-VERSION=''
-
+# Validate argument as version number 
+VERSION=$1
+re='^[0-9]+(\.[0-9]){0,2}$'
+if ! [[$VERSION =~ $re]]
+then
+  echo "ParserAgent build scripts must be called with a valid container version."
+  exit 1
 
 # Local envars
+echo "Initializing local variables"
 APP_PATH=/tmp/build/LengthParser/$(date +%s)
 AGENTS=$WORKING/Agents/ParserAgent
 DATACONN=$WORKING/flask/app/DataConnect
 source $AGENTS/build/helpers.sh
 
 # Create build dir and copy source files
+echo "Creating temporary build files"
 mkdir -p $APP_PATH
 cd $APP_PATH
 cp $DATACONN/Database.py $APP_PATH/
@@ -22,6 +28,7 @@ cp $AGENTS/build/length-parser.Dockerfile $APP_PATH/Dockerfile
 cp $AGENTS/build/length-parser.requirements.txt $APP_PATH/requirements.txt
 
 # Initialize build env and build image
+echo "Building image"
 export $(xargs < $DATACON/.secrets)
 virtualenv $APP_PATH/venv
 source $APP_PATH/venv/bin/activate
@@ -30,8 +37,12 @@ docker build . --tag "LengthParserAgent:$VERSION"
 deactivate
 
 # Tag image and push to AWS ECR
+echo "Pushing image $imageID to ECR repository"
 image_id=`imageID "LengthParserAgent:$VERSION"`
 ecr_tag="$ECR_AGENTS_REPO_URI:LengthParserAgent$VERSION"
 docker tag -t $image_id $ecr_tag
 docker push $ecr_tag
+
+echo "Build completed. I hope the deployment goes well."
+exit 0
 
